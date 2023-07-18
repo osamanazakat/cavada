69fd25bf4a0181a1a4bb2a451a8c5ad219baed44
Osama Nazakat <usamanazakat97@gmail.com>
2023-07-18 11:19:54 +0500

    stats section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Stats from "./components/stats/Stats";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Stats />
     </>
   );
 }
---
+++ src/components/stats/Stats.jsx
@@ -0,0 +1,29 @@
+import "./Stats.scss";
+import Wrapper from "../wrapper/Wrapper";
+
+const Stats = () => {
+  return (
+    <section className="stats">
+      <Wrapper className="stats__container">
+        <div className="stats__point">
+          <h1>10</h1>
+          <p>Shops</p>
+        </div>
+        <div className="stats__point">
+          <h1>06</h1>
+          <p>Years of Experience</p>
+        </div>
+        <div className="stats__point">
+          <h1>120+</h1>
+          <p>Coffees</p>
+        </div>
+        <div className="stats__point">
+          <h1>1302+</h1>
+          <p>Reviews</p>
+        </div>
+      </Wrapper>
+    </section>
+  );
+};
+
+export default Stats;
