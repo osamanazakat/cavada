40417464d89ce466c67ada922185b8cd896aee3e
Osama Nazakat <usamanazakat97@gmail.com>
2023-07-20 12:58:22 +0500

    flower section complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import Stats from "./components/stats/Stats";
 import OpenSection from "./components/openSection/OpenSection";
 import Cta from "./components/cta/Cta";
+import FlowerSection from "./components/flowerSection/FlowerSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Stats />
       <OpenSection />
+      <FlowerSection />
       <Cta />
     </>
   );
